7bbecef2a95c37f47f1159f1f78659825bddb62f
Jakob Pupke <jakop@pm.me>
2020-03-03 17:38:38 +0100

    refactoring

--- app.rb
+++ app.rb
@@ -124,63 +124,76 @@ class App < Sinatra::Base
     settings.redis
   end
 
+  def data_from_comment(comment)
+    content = comment["body"]["content"].map do |c|
+      c["content"]
+    end
+
+    mentions = content.flat_map do |cc|
+      cc.map { |c| c["attrs"]["id"] if c["type"] == "mention" }.compact
+    end
+
+    return nil if mentions.empty?
+
+    text = content.flat_map do |cc|
+      cc.map do |c|
+        case c["type"]
+        when "text" then c["text"]
+        when "mention" then "@#{c['attrs']['text']}"
+        when "inlineCard" then c["attrs"]["url"]
+        when "hardBreak" then "\r\n"
+        else raise StandardError, "Unknown element #{c['type']}"
+        end
+      end
+    end.join.strip
+
+    [mentions.uniq, text]
+  end
+
+  def send_message_to_slack(author:, issue:, mentions:, text:)
+    slack_token = redis.get "SLACK_TOKEN"
+    header = "#{author} mentioned you in " \
+             "<https://nerdgeschoss.atlassian.net/browse/#{issue}|*#{issue}*>."
+
+    mentions.each do |id|
+      sub = redis.hget "subs", id
+      next if sub.nil?
+
+      slack_id = JSON.parse(sub)["slack_id"]
+      resp = HTTParty.post(
+        "https://slack.com/api/chat.postMessage",
+        headers: { "Authorization" => "Bearer #{slack_token}" },
+        body: {
+          channel: slack_id,
+          text: header,
+          attachments: [{ text: text }].to_json
+        }
+      )
+
+      raise StandardError, resp.body unless resp.code.to_s.start_with? "2"
+    end
+  end
+
   def process(issue_id, comment_id)
     Thread.new do
       Raven.capture do
         jira_id = redis.get "JIRA_ID"
         token = redis.get "JIRA_TOKEN"
+
         url = "https://api.atlassian.com/ex/jira/#{jira_id}/rest/api/3/issue/#{issue_id}/comment/#{comment_id}"
         resp = HTTParty.get(url, headers: { "Authorization" => "Bearer #{token}" })
         raise StandardError, resp.body unless resp.code.to_s.start_with? "2"
 
         comment = JSON.parse(resp.body)
+        mentions, text = data_from_comment(comment)
+        next if mentions.nil?
 
-        content = comment["body"]["content"].map do |c|
-          c["content"]
-        end
-
-        mentions = content.flat_map do |cc|
-          cc.map { |c| c["attrs"]["id"] if c["type"] == "mention" }.compact
-        end
-
-        if mentions.any?
-          text = content.flat_map do |cc|
-            cc.map do |c|
-              case c["type"]
-              when "text" then c["text"]
-              when "mention" then "@#{c['attrs']['text']}"
-              when "inlineCard" then c["attrs"]["url"]
-              when "hardBreak" then "\r\n"
-              else "OOPS_UNKNOWN_ELEMENT_IN: #{comment_id}"
-              end
-            end
-          end.join.strip
-
-          header = "#{comment['author']['displayName']} mentioned you in " \
-                   "<https://nerdgeschoss.atlassian.net/browse/#{issue_id}|*#{issue_id}*>."
-          mentions.uniq.each do |id|
-            sub = redis.hget "subs", id
-            next if sub.nil?
-
-            slack_id = JSON.parse(sub)["slack_id"]
-            slack_token = redis.get "SLACK_TOKEN"
-            resp = HTTParty.post(
-              "https://slack.com/api/chat.postMessage",
-              headers: {
-                "Authorization" => "Bearer #{slack_token}"
-              },
-              body: {
-                channel: slack_id,
-                text: header,
-                attachments: [
-                  { text: text }
-                ].to_json
-              }
-            )
-
-            raise StandardError, resp.body unless resp.code.to_s.start_with? "2"
-          end
-        end
+        send_message_to_slack(
+          author: comment["author"]["displayName"],
+          issue: issue_id,
+          mentions: mentions,
+          text: text
+        )
       end
     end
   end
